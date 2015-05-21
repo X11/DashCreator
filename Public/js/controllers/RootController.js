a662b1d62c30f67eac3cbf2f878e5d59f680a446
Ikzelf01 <ikzelf01@live.nl>
2015-05-21 21:38:50 +0200

    Material design fix

--- Public/js/controllers/RootController.js
+++ Public/js/controllers/RootController.js
@@ -1,8 +1,8 @@
 angular.module('dashControllers').controller('RootCtrl', RootCtrl);
 
-RootCtrl.$inject = ['$scope', '$rootScope', '$location', 'AuthenticationService'];
+RootCtrl.$inject = ['$scope', '$rootScope', '$location', '$timeout', 'AuthenticationService'];
 
-function RootCtrl($scope, $r, $location, AuthenticationService){
+function RootCtrl($scope, $r, $location, $timeout, AuthenticationService){
     
     $r.$watch('globals', function(){
         $scope.loggedIn = ($r.globals.currentUser) ? true : false;
@@ -11,9 +11,11 @@ function RootCtrl($scope, $r, $location, AuthenticationService){
     $scope.title = "Dash Creator";
     $scope.description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-    $scope.onViewLoad = function(){
-        $.material.init();
-    };
+    $scope.$on('$viewContentLoaded', function(){
+        $timeout(function(){
+            $.material.init();
+        }, 0);
+    });
 
     $scope.logout = function(){
         AuthenticationService.ClearCredentials();
