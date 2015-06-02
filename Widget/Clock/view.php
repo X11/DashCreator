a8618f88c5252873b77c256975ede2d43f0bda7b
Ikzelf01 <ikzelf01@live.nl>
2015-06-02 14:17:05 +0200

    Needs fix

--- Widget/Clock/view.php
+++ Widget/Clock/view.php
@@ -1,28 +1,3 @@
-<script>
-window.onload = function() {getTime();}
-	// 
-	function getTime() {
-		var time = new Date();
-	    var h = time.getHours();
-	    var m = time.getMinutes();
-	    var s = time.getSeconds();
-	    m = checkTime(m);
-	    s = checkTime(s);
-	    document.querySelector('.clock').innerHTML = h + ":" + m + ":" + s;
-	    var t = setTimeout(function(){getTime()},500);
-	}
-
-
-function checkTime(i) {
-	    if (i<10) {i = "0" + i};  // +0 if <10 //
-	    return i;
-	}
-</script>
-
-<style>
-    /*.clock {font-weight:300;font-size:5em;font-family: 'Lato', sans-serif;}*/
-</style> 
-
 <div class="row text-center">
-	<h2 class="clock clock-widget">00:00:00</h2>
+	<h2 class="clock clock-widget" onload="new C">00:00:00</h2>
 </div>
