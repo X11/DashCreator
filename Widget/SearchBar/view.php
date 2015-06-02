e1dd8f21154543a4c04bbdc0b7d9c510f9f965d7
Farhat Abdad <farhat.a@live.com>
2015-06-02 12:19:46 +0200

    search widget

---
+++ Widget/SearchBar/view.php
@@ -0,0 +1,7 @@
+<style>
+
+</style>
+
+<form  class="navbar-form" action="https://duckduckgo.com/" method="get" target="blank">
+    <input class="searchbox form-control" type="text" name="q" value="" />
+</form>
