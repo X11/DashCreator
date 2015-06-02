0151bf65689d719f3000d59ec26283a1999aaa0a
Finzy <perryzetten@gmail.com>
2015-06-02 15:13:00 +0200

    post-it widget added

---
+++ Widget/Postit/postit.php
@@ -0,0 +1,36 @@
+<div class="postit">
+    <p><input type="text" placeholder="Title Here"></p>
+    <textarea  placeholder="Text here"></textarea>
+</div>
+
+
+<style>
+.postit{
+    text-decoration: none;
+    color: #000;
+    background: #ffc;
+    min-height:100px;
+    padding: 20px;
+    -moz-box-shadow: 5px 5px 7px rgba(33, 33, 33, 1);
+    -webkit-box-shadow: 5px 5px 7px  rgba(33, 33, 33, .7);
+    box-shadow: 5px 5px 7px rgba(33, 33, 33, .7);
+    -moz-transition: -moz-transform .15s linear;
+    -o-transition: -o-transform .15s linear;
+    -webkit-transition: -webkit-transform .15s linear;
+    border-style: solid;
+
+}
+.postit p input{
+    border:none;
+    background: #ffc;
+    width:100%;
+    font-size:20px;
+}
+.postit textarea{
+    width:100%;
+    border:none;
+    background:#ffc;
+    resize:none;
+    overflow:hidden;
+}
+</style>
