b6d5f4e53cbed311b17b6a514d493ed514791043
Ikzelf01 <ikzelf01@live.nl>
2015-06-03 21:40:22 +0200

    Confirm on remove all

---
+++ Public/js/directives/ngConfirmClickDirective.js
@@ -0,0 +1,20 @@
+angular.module('dashDirectives').directive('ngConfirmClick', ngConfirmClick);
+
+ngConfirmClick.$inject = [];
+
+function ngConfirmClick(){
+    return {
+        priority: -1,
+        restrict: 'A',
+        link: function(scope, element, attrs){
+            var message = attrs.ngConfirmClick || "Are you sure";
+            console.log(message);
+            element.bind('click',function (e) {
+                if (!window.confirm(message)){
+                    e.stopImmediatePropagation();
+                    e.preventDefault();
+                }
+            });
+        }
+    };
+}
