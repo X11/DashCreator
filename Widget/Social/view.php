0c47075773fa562259535831d1cae59c24744202
Farhat Abdad <farhat.a@live.com>
2015-06-03 14:16:07 +0200

    added social widget

---
+++ Widget/Social/view.php
@@ -0,0 +1,8 @@
+<div class="text-center">
+    <div class="btn-group-vertical">
+        <a href="http://www.facebook.com/" target="_blank" class="btn btn-default">Facebook</a>
+        <a href="http://www.twitter.com/" target="_blank" class="btn btn-default">Twitter</a>
+        <a href="http://www.youtube.com/" target="_blank" class="btn btn-default">Youtube</a>
+        <a href="http://www.github.com/" target="_blank" class="btn btn-default">Github</a>
+    </div>
+</div>
