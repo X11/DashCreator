792f0480d98fa6de7713bb574f268b2902ac747a
Finzy <perryzetten@gmail.com>
2015-06-03 14:15:11 +0200

    random quotes added

--- Widget/Quote/view.php
+++ Widget/Quote/view.php
@@ -1,6 +1,4 @@
-<div class="row">
-	<blockquote>
-    	<p>Noblety is not about being superior to your fellow men, True noblety lies in being superior to your former self.</p>
-    	<small>Galahad, <cite title="Kingsman">Kingsman</cite></small>
-	</blockquote>
-</div>
+<blockquote>
+    <p>Noblety is not about being superior to your fellow men, True noblety lies in being superior to your former self.</p>
+    <small>Galahad, <cite title="Kingsman">Kingsman</cite></small>
+</blockquote>
